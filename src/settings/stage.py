ea14be912a87bbdb5bc4b884cfcd3870ca7233dc
he_yulin <he_yulin@163.com>
2018-05-03 18:51:14 +0800

    debug 配置文件

--- src/settings/stage.py
+++ src/settings/stage.py
@@ -49,7 +49,7 @@ DATABASES = {
        } ,     
 }
 
-ALLOWED_HOSTS=['192.168.0.244','localhost']
+ALLOWED_HOSTS=['192.168.0.244','103.246.219.202']
 DATABASE_ROUTERS = ['hello.db_router.DbRouter']
 
 MAX_BANNER_SIZE=1024*1024*2
