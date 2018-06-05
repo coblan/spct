bdcc9b9d51f06f8e67aa1bfc5883652e47add121
he_yulin <he_yulin@163.com>
2018-06-05 17:55:43 +0800

    debug

--- src/maindb/admin_games/admin_odds.py
+++ src/maindb/admin_games/admin_odds.py
@@ -170,6 +170,12 @@ class OddsTypeGroup4Table(ModelTable):
 declare @OddsTypeGroup int
 declare @PageIndex int
 declare @PageSize int
+set @PageIndex='%(pageindex)s'
+set @PageSize = '%(pagesize)s'
+set @OddsTypeGroup='4'
+declare @tb_matches table(
+	MatchID bigint
+)
 
 select count(1) as TotalCount from dbo.TB_Matches with(nolock)
 where 1=1 %(where_filter)s
