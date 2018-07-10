419100cd44fd1db36ab52f0dcfc86f22434eeb26
he_yulin <he_yulin@163.com>
2018-07-10 18:48:00 +0800

    debug

--- src/maindb/rabbitmq_instance.py
+++ src/maindb/rabbitmq_instance.py
@@ -4,9 +4,10 @@ rabbitHost = settings.RABBITMQ
 
 credentials = pika.PlainCredentials('publisher', 'publisher')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitHost, credentials = credentials))
-channel = connection.channel()
+
 
 def closeHandicap(msg): 
+    channel = connection.channel()
     channel.basic_publish(exchange='sportative.topic',
                           routing_key= 'match.closehandicap',
                           body=msg)
