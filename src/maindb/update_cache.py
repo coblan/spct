924c1b38c7e94ad03e10a5c916b84d8c388486de
he_yulin <he_yulin@163.com>
2018-08-14 17:19:08 +0800

    debug

--- src/maindb/update_cache.py
+++ src/maindb/update_cache.py
@@ -1,20 +1,24 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from .redisInstance import redisInst
-from .models import TbCurrency, TbNotice, TbBanner, TbMatches
+#from .models import TbCurrency, TbNotice, TbBanner, TbMatches, TbOddstypegroup
+from . import models as sports_model
 
-dc = {
-    TbCurrency: 'App:Static:Currency',
-    TbNotice: 'App:Cache:index:notices',
-    TbBanner: 'App:Cache:index:banners',
-    TbMatches: 'App:Cache:index:matches',
-    
-}
+#dc = {
+    #TbCurrency: 'App:Static:Currency',
+    #TbNotice: 'App:Cache:index:notices',
+    #TbBanner: 'App:Cache:index:banners',
+    #TbMatches: 'App:Cache:index:matches',
+    #TbOddstypegroup: 'App:Static:TypeGroup',
+#}
 
 
 def update_redis_cache(sender, **kws): 
-    if sender in dc:
-        redisInst.delete(dc.get(sender))  
+    if sender.__module__ == 'maindb.models' :  #in sports_model:
+        redisInst.delete('App:Cache:index:*')
+        redisInst.delete('App:Static:*')
+    #if sender in dc:
+        #redisInst.delete(dc.get(sender))  
         
 
 post_delete.connect(update_redis_cache)
