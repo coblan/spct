7542fbadc834d382532dfa9a92f6faf5d5a06254
hc <hc@jingbo360.com>
2018-09-12 10:18:36 +0800

    u

--- src/maindb/riskcontrol/admin.py
+++ src/maindb/riskcontrol/admin.py
@@ -4,8 +4,8 @@ from . import max_payout
 from helpers.director.access.permit_data import model_full_permit, model_to_name, add_permits
 
 permits = [
-    ('TbBlackuserlist', model_full_permit(TbBlackuserlist), model_to_name(TbBlackuserlist), 'model'),
-    ('TbBlackuserlistLog', model_full_permit(TbBlackuserlistLog), model_to_name(TbBlackuserlistLog), 'model'),
+    # ('TbBlackuserlist', model_full_permit(TbBlackuserlist), model_to_name(TbBlackuserlist), 'model'),
+    # ('TbBlackuserlistLog', model_full_permit(TbBlackuserlistLog), model_to_name(TbBlackuserlistLog), 'model'),
     ('Blackiprangelist', model_full_permit(Blackiprangelist), model_to_name(Blackiprangelist), 'model'),
     ('Whiteiplist', model_full_permit(Whiteiplist), model_to_name(Whiteiplist), 'model'),
     ('Whiteuserlist', model_full_permit(Whiteuserlist), model_to_name(Whiteuserlist), 'model'),
