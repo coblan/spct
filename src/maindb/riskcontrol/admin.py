86ff63f7b9c10d658774968e4cf62f66b032f1c7
he_yulin <he_yulin@163.com>
2018-09-12 10:43:18 +0800

    debug

--- src/maindb/riskcontrol/admin.py
+++ src/maindb/riskcontrol/admin.py
@@ -4,6 +4,7 @@ from . import max_payout
 from helpers.director.access.permit_data import model_full_permit, model_to_name, add_permits
 
 permits = [
-    ('Whiteuserlist', model_full_permit(Whiteuserlist), model_to_name(Whiteuserlist), 'model'),]
+    #('Whiteuserlist', model_full_permit(Whiteuserlist), model_to_name(Whiteuserlist), 'model'),
+]
 
 add_permits(permits)
