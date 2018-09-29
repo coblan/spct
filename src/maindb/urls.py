888257d42f599dad2608b8369513bf7ccc19ba6b
he_yulin <he_yulin@163.com>
2018-09-29 16:50:01 +0800

    debug

--- src/maindb/urls.py
+++ src/maindb/urls.py
@@ -2,12 +2,12 @@
 """
 from django.conf.urls import include, url
 
-from . import  rec_file
+#from . import  rec_file
 from . import views
 
 
 urlpatterns = [ 
-    url(r'^upload/?$',rec_file.receive_banner,name='banner_upload'),
+    #url(r'^upload/?$',rec_file.receive_banner,name='banner_upload'),
     url(r'^app_upload/?$',views.recieve_app_pkg,name='app_pkg_upload'),
     url(r'^ckeditor_img/?$',views.recieve_ckeditor_img,name='ckeditor_img')
 ]
