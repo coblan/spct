d3a51011f792331d780c418f50bd16ba52df3f9e
he_yulin <he_yulin@163.com>
2018-12-25 10:36:29 +0800

    debug

--- src/static/js/director.pack.js
+++ src/static/js/director.pack.js
@@ -3456,8 +3456,8 @@ var multi_chosen = {
     template: '<select  multiple="multiple" class="multi-chosen form-control">\n    <option v-for="option in options" :value="option.value" v-text="option.label"></option>\n</select>',
     mounted: function mounted() {
         var self = this;
-        ex.load_css('https://cdn.bootcss.com/chosen/1.8.2/chosen.min.css');
-        ex.load_js('https://cdn.bootcss.com/chosen/1.8.2/chosen.jquery.min.js', function () {
+        ex.load_css(cfg.js_lib.chosen_css);
+        ex.load_js(cfg.js_lib.chosen).then(function () {
             $(self.$el).chosen({
                 search_contains: true
             }).change(function (event) {
@@ -3466,6 +3466,16 @@ var multi_chosen = {
             self.setValue(self.value);
             $(self.$el).siblings('.chosen-container').removeAttr('style');
         });
+        //ex.load_css('https://cdn.bootcss.com/chosen/1.8.2/chosen.min.css')
+        //ex.load_js('https://cdn.bootcss.com/chosen/1.8.2/chosen.jquery.min.js',function(){
+        //    $(self.$el).chosen({
+        //        search_contains:true
+        //    }).change(function(event){
+        //        self.$emit('input',$(this).val())
+        //    });
+        //    self.setValue(self.value)
+        //    $(self.$el).siblings('.chosen-container').removeAttr('style')
+        //})
     },
     watch: {
         value: function value(nv) {
