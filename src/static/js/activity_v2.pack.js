42d70cb306c2adb7956f657a807728706ceb5945
he_yulin <he_yulin@163.com>
2019-01-16 18:29:50 +0800

    debug

--- src/static/js/activity_v2.pack.js
+++ src/static/js/activity_v2.pack.js
@@ -537,7 +537,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 __webpack_require__(32);
 
 Vue.component('com-shouchun', {
-    template: '<div class="com-shouchun">\n\n    <!--<button @click="update_data()">\u83B7\u53D6</button>-->\n\n    <table>\n    <tr v-for="row in rows">\n        <td v-for="head in heads"  :class="head.scls">\n            <div   v-if="row[head.name]" v-text="head.top_label"></div>\n            <div v-text="row[head.name]"></div>\n        </td>\n        <td class="mybtn-col">\n            <div :class="[\'mybtn\',{disabled:!row.submitable}]" @click="submit(row)"><span class="center-vh" style="white-space: nowrap">\u53C2\u52A0\u6D3B\u52A8</span></div>\n        </td>\n    </tr>\n    </table>\n    </div>',
+    template: '<div class="com-shouchun">\n\n    <!--<button @click="update_data()">\u83B7\u53D6</button>-->\n\n    <table>\n    <tr v-for="row in rows">\n        <td v-for="head in heads"  :class="head.scls">\n            <div   v-if="row[head.name]" v-text="head.top_label"></div>\n            <div v-text="row[head.name]"></div>\n        </td>\n        <td class="mybtn-col">\n            <div :class="[\'mybtn\',{disabled:!row.submitable}]" @click="submit(row)"><span class="center-vh" style="white-space: nowrap" v-text="action_label(row)"></span></div>\n        </td>\n    </tr>\n    </table>\n    </div>',
     data: function data() {
         return {
             heads: [{ name: 'label', scls: 'big-col', top_label: '' }, { name: 'ChargeTime', scls: 'data-col', top_label: '存款' }, { name: 'Amount', scls: 'data-col', top_label: '存入' }, { name: 'Bonus', scls: 'data-col green', top_label: '可得红利' }],
@@ -557,10 +557,18 @@ Vue.component('com-shouchun', {
         //},5000)
     },
     methods: {
+        action_label: function action_label(row) {
+            var dc = {
+                0: '参加活动',
+                1: '已参加',
+                2: '已发放'
+            };
+            return dc[row.State];
+        },
         update_data: function update_data() {
             //cfg.showMsg('开始更新数据')
             var mock_data = {
-                data: [{ ChargeTime: '04-21 22:30', Amount: 50, Bonus: 50, Done: false }]
+                data: [{ ChargeTime: '04-21 22:30', Amount: 50, Bonus: 50, State: 2 }, { ChargeTime: '2019-01-21 22:30:30', Amount: '100000', Bonus: '1239999', State: 1 }]
             };
             var dec_rows = [{ label: '首存', action: '', submitable: false }, { label: '再存', action: '', submitable: false }];
             var self = this;
@@ -575,7 +583,7 @@ Vue.component('com-shouchun', {
                     var row = self.rows[i];
                     ex.vueAssign(row, dec_rows[i]);
                     row.Type = i + 1;
-                    if (row.Done) {
+                    if (row.State != 0) {
                         last_done = true;
                     } else if (last_done) {
                         row.submitable = true;
