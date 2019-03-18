0650dda92e8bacfaa7a415df93aed179cc0b3534
he_yulin <he_yulin@163.com>
2019-03-18 15:46:09 +0800

    debug

--- src/maindb/member/chum_user.py
+++ src/maindb/member/chum_user.py
@@ -1,4 +1,4 @@
-from helpers.director.shortcut import TablePage,ModelTable,page_dc,director
+from helpers.director.shortcut import TablePage,ModelTable,page_dc,director,RowFilter,SelectSearch
 from ..models import TbAccount 
 from django.core.exceptions import PermissionDenied
 from helpers.director.access.permit import has_permit
@@ -17,7 +17,20 @@ class ChumUser(TablePage):
     
     class tableCls(ModelTable):
         model = TbAccount
-        exclude=['password','fundspassword']
+        exclude=['password','fundspassword','agent','pwupdatetime','avatar',
+                 'gender','birthday','points','codeid','parentid','isriskleveldown','phone','cashchannel']
+        
+        def dict_head(self, head):
+            width_dc={
+                'nickname':180,
+                'account':150,
+                'createtime':150,
+                'amount':130,
+                
+            }
+            if width_dc.get(head['name']):
+                head['width']=width_dc.get(head['name'])
+            return head
         
         def inn_filter(self, query):
             return query.filter(sumrechargecount__lte=1)
@@ -26,6 +39,33 @@ class ChumUser(TablePage):
             return [
                 {'fun': 'export_excel', 'editor': 'com-op-btn', 'label': '导出Excel', 'icon': 'fa-file-excel-o', }
             ]
+        
+        class filters(RowFilter):
+            names=['accounttype','groupid']
+            range_fields = ['createtime']   
+        
+        class search(SelectSearch):
+            names = ['nickname']
+            exact_names = ['accountid']
+
+            def get_option(self, name):
+                if name == 'accountid':
+                    return {'value': name,
+                            'label': '账户ID', }
+                elif name == 'nickname':
+                    return {
+                        'value': name,
+                        'label': '昵称',
+                    }
+
+            def clean_search(self):
+                if self.qf in ['accountid']:
+                    if not re.search('^\d*$', self.q):
+                        return None
+                    else:
+                        return self.q
+                else:
+                    return super().clean_search()        
 
 director.update({
     'chum_user':ChumUser.tableCls
