e4483aeee2b8ed2bf40a35ed210279f192f1eb8e
he_yulin <he_yulin@163.com>
2019-03-21 18:06:01 +0800

    修改 相关用户

--- src/maindb/member/relevent_user.py
+++ src/maindb/member/relevent_user.py
@@ -25,27 +25,30 @@ class ReleventUserPage(TablePage):
                  'ctx_name':'account_tabs',
                  'tab_name':'baseinfo'},              
                 {'name':'NickName','label':'昵称','width':140},
-                {'name':'CreateTime','label':'注册时间','width':150},
-                #{'name':'Source','label':'来源'},
-                #{'name':'CreateTime','label':'注册时间'},
-                #{'name':'Weight','label':'权重'},
-                #{'name':'GroupID','label':'用户组'},
-                #{'name':'VIPLv','label':'vip等级'},
-                #{'name':'Status','label':'用户状态'},
-                {'name':'IsEnableWithdraw','label':'允许提现','editor':'com-table-bool-shower'},
-                {'name':'Amount','label':'余额'},
+                {'name':'Relevant','label':'关联条件','width':150},
+                {'name':'Reason','label':'原因',},
                 
             ]
         
         def getRowFilters(self):
             return [
-                    {'name':'NickName','label':'昵称','editor':'com-filter-text'},
-                    {'name':'DeviceCode','label':'设备码','editor':'com-filter-text'},
-                    {'name':'IpAddress','label':'IP地址','editor':'com-filter-text'},
-                    {'name':'CardNo','label':'银行卡号','editor':'com-filter-text'},
-                    {'name':'CardOwner','label':'卡Owner','editor':'com-filter-text'},
-                ]
-            
+                {'name':'relevent','label':'查询条件','editor':'com-filter-select',
+                 'options':[{'value':1,'label':'设备ID'},
+                            {'value':2,'label':'IP地址'},
+                            {'value':3,'label':'持卡人'}]}
+            ]
+            #return [
+                    #{'name':'NickName','label':'昵称','editor':'com-filter-text'},
+                    #{'name':'DeviceCode','label':'设备码','editor':'com-filter-text'},
+                    #{'name':'IpAddress','label':'IP地址','editor':'com-filter-text'},
+                    #{'name':'CardNo','label':'银行卡号','editor':'com-filter-text'},
+                    #{'name':'CardOwner','label':'卡Owner','editor':'com-filter-text'},
+                #]
+                
+        @classmethod
+        def clean_search_args(cls, search_args):
+            search_args['relevent'] = search_args.get('relevent') or 1
+            return search_args
         
         def get_rows(self):
             rows = self.get_data_from_db()
@@ -55,16 +58,18 @@ class ReleventUserPage(TablePage):
             rows=[]
             nickname = self.search_args.get('NickName','')
             sql_args={
-                'NickName':self.search_args.get('NickName',''),
-                'DeviceCode':self.search_args.get('DeviceCode',''),
-                'IpAddress':self.search_args.get('IpAddress',''),
-                'CardNo':self.search_args.get('CardNo',''),
-                'CardOwner':self.search_args.get('CardOwner',''),
+                'relevent':self.search_args.get('relevent',1)
+                #'NickName':self.search_args.get('NickName',''),
+                #'DeviceCode':self.search_args.get('DeviceCode',''),
+                #'IpAddress':self.search_args.get('IpAddress',''),
+                #'CardNo':self.search_args.get('CardNo',''),
+                #'CardOwner':self.search_args.get('CardOwner',''),
             }
-            sql = r"exec dbo.SP_RelevantUser %%s,'%(DeviceCode)s','%(IpAddress)s','%(CardNo)s','%(CardOwner)s'" \
+            
+            sql = r"exec dbo.SP_RelevantUser %(relevent)s" \
                   % sql_args
             with connections['Sports'].cursor() as cursor:
-                cursor.execute(sql, [nickname])
+                cursor.execute(sql)
                 for row in cursor:
                     dc = {}
                     for index, head in enumerate(cursor.description):
