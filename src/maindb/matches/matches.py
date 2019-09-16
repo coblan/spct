0ee864b6d7f0765920306d7cbf6ceafb30f83e9a
he_yulin <he_yulin@163.com>
2019-09-16 11:16:38 +0800

    debug

--- src/maindb/matches/matches.py
+++ src/maindb/matches/matches.py
@@ -1359,10 +1359,8 @@ def out_com_save(rows,matchid):
                     
         statuscode_list = [x.statuscode for x in batch_create]
         
-        
-        match.score = '%s:%s'%(home_score,away_score)
         if 100 in statuscode_list:
-            
+            match.score = '%s:%s'%(home_score,away_score)
             if home_score <away_score:
                 match.winner = 2
             elif home_score > away_score:
@@ -1373,15 +1371,15 @@ def out_com_save(rows,matchid):
             match.marketstatus=3
             match.statuscode = 100
             match.terminator ='manual'
-        match.save()
-        dc={
-            'MatchID':match.matchid, 
-            'Score':match.score,
-            'MarketStatus':match.marketstatus,
-            'StatusCode':match.statuscode,
-            'Terminator':match.terminator
-        }
-        updateMatchMongo(dc)
+            match.save()
+            dc={
+                'MatchID':match.matchid, 
+                'Score':match.score,
+                'MarketStatus':match.marketstatus,
+                'StatusCode':match.statuscode,
+                'Terminator':match.terminator
+            }
+            updateMatchMongo(dc)
 
     notifyManulOutcome(json.dumps(send_dc))
 
