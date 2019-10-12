689c2aba043955e544499665305c11b3b111857a
he_yulin <he_yulin@163.com>
2019-10-12 18:32:20 +0800

    debug

--- src/maindb/member/chum_user.py
+++ src/maindb/member/chum_user.py
@@ -95,7 +95,7 @@ class ChumUser(TablePage):
                 elif self.sort_str =='-sleep_days':
                     return  query.order_by('lastbettime')
                 else:
-                    return query
+                    return super().get_query(query)
                 
         class filters(RowFilter):
             names=['accounttype','groupid','source','sumrechargecount']
