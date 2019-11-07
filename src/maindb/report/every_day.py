aafd6c91a64d73b131fa05ed2fe85db9174adc08
he_yulin <he_yulin@163.com>
2019-11-07 10:30:51 +0800

    增加每日报表统计和导出excel

--- src/maindb/report/every_day.py
+++ src/maindb/report/every_day.py
@@ -1,5 +1,6 @@
 from helpers.director.shortcut import ModelTable,TablePage,director,page_dc,RowFilter,RowSort
 from maindb.models import TbTrendstatistics
+from django.db.models import Sum,Q
 
 class EveryDayReportPage(TablePage):
     def get_label(self):
@@ -35,6 +36,36 @@ class EveryDayReportPage(TablePage):
                 head['class'] = 'scope.row[scope.head.name]<0?"everyday-warning":""'
                 head['css'] = '.everyday-warning{background-color:green;color:white}'
             return head
+        
+        def get_operation(self):
+            return [
+                {'label':'导出excel','editor':'com-op-btn','action':'scope.ps.export_excel()'}
+            ]
+        
+        def statistics(self, query):
+            sum_list = ['betnum','betamount','finishbetamount','turnover','betoutcome','bonusamount','firstrechargeamount','secondrechargeamount',
+                        'birthdayamount','backendamount','userprofit','activityamount','rechargeamount','withdrawamount','betusernum','newusernum','withdrawusernum','rechargeusernum']
+            filters={}
+            for item in sum_list:
+                filters['total_%s'%item] = Sum(item)
+          
+            dc = query.aggregate(**filters)
+            self.footer = {'_label':'合计'}
+            for item in sum_list:
+                self.footer[item] = dc.get('total_%s'%item)
+                
+            #dc = query.aggregate(total_betnum=Sum('betnum'),total_betamount=Sum('betamount'),total_finishbetamount=Sum('finishbetamount'),
+                                 #total_turnover=Sum('turnover'),total_betoutcome=Sum('betoutcome'),total_bonusamount=Sum('bonusamount'),
+                                 #total_firstrechargeamount=Sum('firstrechargeamount'))
+            #mapper = {
+                #'turnover': 'total_turnover'
+            #}
+            #for k in dc:
+                #dc[k] = str(round(dc.get(k) or 0, 2))
+            #self.footer = {'betnum':dc.get('total_betnum'),'betamount':dc.get('total_betamount'),'finishbetamount':dc.get('total_finishbetamount'),
+                           #'turnover':dc.get('total_turnover'),'betoutcome':dc.get('total_betoutcome'),
+                           #'bonusamount':dc.get('total_bonusamount'),'firstrechargeamount':dc.get('total_firstrechargeamount'),'_label':'合计'}
+            return query
         
         class filters(RowFilter):
             range_fields=['starttime']
