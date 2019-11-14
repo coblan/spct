c4728fda07b17eb1d5f465d5f061ba8c3100dff3
he_yulin <he_yulin@163.com>
2019-11-14 16:44:09 +0800

    dd

--- src/maindb/matches/event_match1.py
+++ src/maindb/matches/event_match1.py
@@ -20,11 +20,15 @@ operation_log = logging.getLogger('operation_log')
 beijin = datetime.timezone(datetime.timedelta(hours=8))
 utc = datetime.timezone(datetime.timedelta(hours=0))
 def tm2mongo(dt):
+    if not dt:
+        return dt
     tmp = dt.replace(tzinfo=beijin)
     #return tmp.astimezone(beijin)
     return tmp
 
 def mongo2tm(dt):
+    if not dt:
+        return dt
     dd = dt.replace(tzinfo=utc)
     return dd.astimezone(beijin)
 
