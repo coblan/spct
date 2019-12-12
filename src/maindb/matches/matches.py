bb4accb2b32d894ca8944999dd6c95f30089a7d4
he_yulin <he_yulin@163.com>
2019-12-12 14:40:18 +0800

    debug

--- src/maindb/matches/matches.py
+++ src/maindb/matches/matches.py
@@ -1192,28 +1192,30 @@ def out_com_save(rows,matchid):
             dc = {}
             has_overtime = row.pop('has_overtime',False)
             has_half2 = row.pop('has_half2',False)
-            
+
             TbPeriodscore.objects.filter(matchid=matchid,scoretype=1).delete()
 
             home_6_1 = int( row.get('home_6_1') )
             away_6_1 = int( row.get('away_6_1') )
             batch_create.append( TbPeriodscore(matchid=matchid,statuscode=6,scoretype=1,home=home_6_1,away=away_6_1 ,type=0) )
             if  has_half2 :
-                home_100_1 = int( row.get('home_100_1') )
-                away_100_1 = int( row.get('away_100_1') )
-    
+               
                 home_7_1 = home_100_1 - home_6_1
                 away_7_1 = away_100_1 - away_6_1
                 batch_create.append( TbPeriodscore(matchid=matchid,statuscode=7,scoretype=1,home=home_7_1,away=away_7_1 ,type=0) )
-                batch_create.append( TbPeriodscore(matchid=matchid,statuscode=100,scoretype=1,home=home_100_1,away=away_100_1 ,type=0) )
+                if row.get('has_100_1'):
+                    home_100_1 = int( row.get('home_100_1') )
+                    away_100_1 = int( row.get('away_100_1') )
+                    batch_create.append( TbPeriodscore(matchid=matchid,statuscode=100,scoretype=1,home=home_100_1,away=away_100_1 ,type=0) )
 
             if has_overtime:
                 home_40_1 = int( row.pop('home_40_1') )
                 away_40_1 = int( row.pop('away_40_1') )
-                home_110_1 = home_100_1 + home_40_1
-                away_110_1 = away_100_1 + away_40_1
                 batch_create.append(TbPeriodscore(matchid=matchid,statuscode=40,scoretype=1,type=1,home=home_40_1,away=away_40_1)) 
-                batch_create.append(TbPeriodscore(matchid=matchid,statuscode=110,scoretype=1,type=1,home=home_110_1,away=away_110_1)) 
+                if row.get('has_100_1'):
+                    home_110_1 = home_100_1 + home_40_1
+                    away_110_1 = away_100_1 + away_40_1
+                    batch_create.append(TbPeriodscore(matchid=matchid,statuscode=110,scoretype=1,type=1,home=home_110_1,away=away_110_1)) 
                 
         if row['pk'] == -109:
             send_dc['IsSettleByScore']=True
