e943ba93639fbb9b28e242cd1c743de6320a1929
he_yulin <he_yulin@163.com>
2019-12-23 10:14:33 +0800

    去掉排序

--- src/maindb/report/user_statistics.py
+++ src/maindb/report/user_statistics.py
@@ -109,7 +109,7 @@ class UserStatisticsPage(TablePage):
             names = ['Amount', 'GameCoinRechargeAmount', 'CommissionRechargeAmount', 'ReservedAmount',
                      'CommissionWithDrawAmount', 'BetAmount', 'Turnover', 'BetOutcome', 'BetBonus', 'OrderCount',
                      'WinCount', 'WinRate', 'FirstRechargeBonus', 'SecondRechargeBonus', 'RescueBonus', 'BirthdayBonus',
-                     'AdjustAmount', 'Profit','ProfitRate','WinDayRate','WinDay','TotalDay','GroupName']
+                     'AdjustAmount', 'Profit','ProfitRate','WinDayRate','WinDay','TotalDay']
             
         def get_rows(self):
             self.getData()
