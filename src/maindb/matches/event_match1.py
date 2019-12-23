df27ea398bf2c8ca3a616f45a4d361f8be4a502d
he_yulin <he_yulin@163.com>
2019-12-23 10:21:09 +0800

    debug

--- src/maindb/matches/event_match1.py
+++ src/maindb/matches/event_match1.py
@@ -381,7 +381,7 @@ class NewMatchForm(ModelFields):
             
         }
         notifyCreateNewMatch(json.dumps([dc],cls=DirectorEncoder,ensure_ascii=False))
-        operation_log.info('创建比赛 %s'%json.dumps(dc))
+        operation_log.info('创建比赛 %s'%json.dumps(dc,cls=DirectorEncoder,ensure_ascii=False))
     
 @director_view('get_league_options')
 def get_league_options(sportid):
