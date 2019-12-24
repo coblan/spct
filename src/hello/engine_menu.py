bbcc6e124de609da4352fc5b8208cc9fd220aeb8
he_yulin <he_yulin@163.com>
2019-12-24 18:34:45 +0800

    debug

--- src/hello/engine_menu.py
+++ src/hello/engine_menu.py
@@ -108,7 +108,7 @@ class PcMenu(BaseEngine):
                  {'label': '注单列表', 'url': page('tickets'),
                   'visible': can_touch(TbTicketmaster, crt_user), },
                 {'label':'调水模板','url':page('adjusttemplate'),'visible':can_touch(TbAdjusttemplate,crt_user)},
-                {'label':'赛事匹配','url':page('web_match_data')},
+                #{'label':'赛事匹配','url':page('web_match_data')},
                 {'label':'赛事匹配V2','url':page('web_match_data1')},
                 {'label':'跟水设置','url':page('mapping-setting')},
                  
