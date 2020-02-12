7ea2810a2e62a0f33138ad749befd964607ec04c
he_yulin <he_yulin@163.com>
2020-02-12 19:27:15 +0800

    debug

--- src/maindb/marketing/message_push.py
+++ src/maindb/marketing/message_push.py
@@ -3,6 +3,7 @@ from maindb.models import TbMessage,TbMessageReceiver,TbMessagetype,TbAccount
 from django.utils.html import strip_tags
 import jpush
 from django.conf import settings
+from helpers.func.collection.mylist import split_list
 import logging
 general_log = logging.getLogger('general_log')
 
@@ -29,13 +30,17 @@ class MessageForm(ModelFields):
     def clean_save(self):
         self.instance.sender = self.crt_user.username
         self.instance.abstract = strip_tags( self.instance.content)[:50]
-
+        
+    def clean(self):
+        if self.instance.typeid.needread == True:
+            if not self.instance.userids and not self.instance.usergroupids and not self.instance.vipgroupids:
+                raise UserWarning('非广播类型消息，必须选择筛选条件')
+        
     def after_save(self):
         if self.instance.sendway ==0 :
             self.instance.refresh_from_db()
             if 'issent' in self.changed_data and self.instance.issent:
                 if self.instance. typeid . needread :
-                   
                     send_user_message(self.instance)
                 else:
                     broad_message(self.instance)
@@ -70,7 +75,7 @@ def send_user_message(inst):
     if inst.vipgroupids:
         vipgroupids= inst.vipgroupids.split(';')
         query = query.filter(viplv__in =  vipgroupids,)
-    userids = [account.accountid for account in query]
+    userids = [str( account.accountid ) for account in query]
     jiguang_push_message(userids, inst.title)
 
 def broad_message(inst):
@@ -81,15 +86,15 @@ def dispatch_message(inst):
     if inst.userids:
         userids = inst.userids.split(';')
         reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = userid,receivertype=1, ) for userid in userids]
-        total_list.append(reciever_insts)
+        total_list.append(*reciever_insts)
     if inst.usergroupids:
         groupids = inst.usergroupids
         reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=2, ) for groupid in groupids]
-        total_list.append(reciever_insts)
+        total_list.append(*reciever_insts)
     if inst.vipgroupids:
         vipgroupids = inst.vipgroupids
         reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=3, ) for groupid in vipgroupids]
-        total_list.append(reciever_insts)
+        total_list.append(*reciever_insts)
         
     TbMessageReceiver.objects.bulk_create(total_list)
 
@@ -111,22 +116,20 @@ def jiguang_broad_message(msg):
     
 def jiguang_push_message(uids,msg):
     app_key,master_secret = settings.JPUSH.get('app_key'),settings.JPUSH.get('master_secret')
-    _jpush = jpush.JPush(app_key, master_secret)
-    push = _jpush.create_push()
-    #single_payload_list = [
-        #{'platform':'all', 'audience':{'alias':uids}, 'notification':{'alert':msg}},
-        ##{'platform':'all', 'target':'regid2', 'notification':{'alert':'alert content'}}
-    #]
-    #response = push.batch_push_by_regid(single_payload_list)
-    
-    push.audience = jpush.audience(
-                jpush.alias(*uids)
-            )
-    push.notification = jpush.notification(alert=msg)
-    push.platform = jpush.all_
-    response=push.send()
-    general_log.debug('推送消息:uids=%s ;返回结果: %s'%(uids,response))
-    print(response)
+    for batch_uids in split_list(uids, 1000):
+        _jpush = jpush.JPush(app_key, master_secret)
+        push = _jpush.create_push()
+        push.audience = jpush.audience(
+                    jpush.alias(*batch_uids)
+                )
+        push.notification = jpush.notification(alert=msg)
+        push.platform = jpush.all_
+        try:
+            response=push.send()
+            general_log.debug('推送消息:uids=%s ;返回结果: %s'%(batch_uids,response))
+            print(response)
+        except jpush.common.JPushFailure as e:
+            general_log.debug('推送消息:uids=%s ;返回结果报错: %s'%(batch_uids,e))
  
 director.update({
     'message':MessagePage.tableCls,
