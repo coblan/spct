e207352d6003fffff3a9b56af6fb5cab89e1d3d9
he_yulin <he_yulin@163.com>
2020-02-21 16:20:51 +0800

    debug

--- src/maindb/marketing/message_push.py
+++ src/maindb/marketing/message_push.py
@@ -174,15 +174,15 @@ def dispatch_message(inst):
     if inst.typeid . needread:
         if inst.userids:
             userids = inst.userids.split(';')
-            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = userid,receivertype=1, ) for userid in userids]
+            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = userid,receivertype=1, ) for userid in userids if userid !='']
             total_list += reciever_insts 
         if inst.usergroupids:
             groupids = inst.usergroupids.split(';')
-            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=2, ) for groupid in groupids]
+            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=2, ) for groupid in groupids if groupid !='']
             total_list += reciever_insts 
         if inst.vipgroupids:
             vipgroupids = inst.vipgroupids.split(';')
-            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=3, ) for groupid in vipgroupids]
+            reciever_insts = [TbMessageReceiver(messageid=inst.pk,receiverid = groupid,receivertype=3, ) for groupid in vipgroupids if groupid !='']
             total_list += reciever_insts
     else:
         total_list.append(TbMessageReceiver(messageid=inst.pk,receiverid = 0,receivertype=0,))
