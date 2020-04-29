966f53518f90fc1d7e346a14a2972c9a5f936be7
he_yulin <he_yulin@163.com>
2020-04-29 17:23:24 +0800

    dd

--- src/maindb/js/activity_v2/main.js
+++ src/maindb/js/activity_v2/main.js
@@ -2,14 +2,16 @@ import * as shou_cun from  './shou_cun.js'
 import * as network from  './network.js'
 
 cfg.showMsg = (msg,title)=>{
-    vant.Dialog.alert({
-        title: title ||  '信息提示',
-        message: msg,
-    })
+    //vant.Dialog.alert({
+    //    title: title ||  '信息提示',
+    //    message: msg,
+    //})
+    return MINT.MessageBox.alert(msg)
 }
 cfg.showError = (msg,title)=>{
-    vant.Dialog.alert({
-        title: title ||  '错误提示',
-        message: msg,
-    })
+    //vant.Dialog.alert({
+    //    title: title ||  '错误提示',
+    //    message: msg,
+    //})
+    return MINT.MessageBox.alert(msg)
 }
