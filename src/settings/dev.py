b84c548031f6be0c2d890834f9d5dfcfa458339a
he_yulin <he_yulin@163.com>
2020-06-24 17:36:36 +0800

    dd

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -69,13 +69,13 @@ DATABASES = {
         'ENGINE': 'sql_server.pyodbc',
         'HOST':'192.168.40.165,1433',
         
-        'NAME': 'Sports_20190829_Part1', #'Sports',#'Sports', 
-        'USER':  'develop_admin',#'Backend'  ,  develop
-        'PASSWORD': 'develop_admin_cheer123', #'SSDEVdev@123',  develop_cheer
+        #'NAME': 'Sports_20190829_Part1', #'Sports',#'Sports', 
+        #'USER':  'develop_admin',#'Backend'  ,  develop
+        #'PASSWORD': 'develop_admin_cheer123', #'SSDEVdev@123',  develop_cheer
         
-        #'NAME':'Sports_2020',
-        #'USER':  'develop',#'Backend'  ,  
-        #'PASSWORD': 'develop_cheer', #'SSDEVdev@123',  
+        'NAME':'Sports_2020',
+        'USER':  'develop',#'Backend'  ,  
+        'PASSWORD': 'develop_cheer', #'SSDEVdev@123',  
         
         #'PORT': '1436',
         #192.168.40.6 Sports_20181214_2
@@ -91,12 +91,12 @@ DATABASES = {
     'Sports_nolock': { 
         'ENGINE': 'sql_server.pyodbc',
         'HOST':'192.168.40.165,1433',
-        'NAME': 'Sports_20190829_Part1', #'Sports',#'Sports', 
-        'USER':  'develop_admin',#'Backend'  ,  develop
-        'PASSWORD': 'develop_admin_cheer123', #'SSDEVdev@123',  develop_cheer
-        #'NAME':'Sports_2020',
-        #'USER':  'develop',#'Backend'  ,  
-        #'PASSWORD': 'develop_cheer', #'SSDEVdev@123',  
+        #'NAME': 'Sports_20190829_Part1', #'Sports',#'Sports', 
+        #'USER':  'develop_admin',#'Backend'  ,  develop
+        #'PASSWORD': 'develop_admin_cheer123', #'SSDEVdev@123',  develop_cheer
+        'NAME':'Sports_2020',
+        'USER':  'develop',#'Backend'  ,  
+        'PASSWORD': 'develop_cheer', #'SSDEVdev@123',  
         #'PORT': '1436',
         #192.168.40.6 Sports_20181214_2
         'OPTIONS': {
