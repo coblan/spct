63cebb757dc73f5a4fda3da88add0e42c384a65c
he_yulin <he_yulin@163.com>
2020-08-29 12:56:51 +0800

    dd

--- src/maindb/part3/im/im_profitloss.py
+++ src/maindb/part3/im/im_profitloss.py
@@ -1,7 +1,8 @@
 from helpers.director.shortcut import TablePage,ModelTable,page_dc,director,RowFilter
 from ..ag.profitloss import AgprofitlossPage
-from maindb.models import TbImprofitloss
+from maindb.models import TbImprofitloss,TbMerchants
 from django.db.models import Sum
+
 
 class IMProfitlossPage(TablePage):
     def get_label(self):
@@ -27,9 +28,29 @@ class IMProfitlossPage(TablePage):
             self.footer = normed_dc
             return query
         
-        #class filters(RowFilter):
-            #names = ['productid']
-            #range_fields=['profitlosstime']
+        class filters(RowFilter):
+            range_fields=['profitlosstime']
+            
+            @property
+            def names(self):
+                if self.crt_user.merchant:
+                    return ['gametype']
+                else:
+                    return ['account__merchant_id','gametype']
+                
+            def getExtraHead(self):
+                if self.crt_user.merchant:
+                    return []
+                else:
+                    return [{
+                        'name':'account__merchant_id','label':'商户','editor':'com-filter-select','options':[
+                            {'value':x.pk,'label':str(x)} for x in TbMerchants.objects.all()
+                        ]
+                     }]
+            #class filters(RowFilter):
+                #names = ['productid']
+                #range_fields=['profitlosstime']
+                ##gametype
 
 
 director.update({
